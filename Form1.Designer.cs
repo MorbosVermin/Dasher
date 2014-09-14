6e1b50614d7ae16b7a6b330d8db99f4c29ec3d2e
Mike Duncan <morbos.vermin@gmail.com>
2014-09-13 20:30:37 -0400

    Icons switch

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -55,9 +55,11 @@
             this.groupBox1.Controls.Add(this.radioButton2);
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Location = new System.Drawing.Point(17, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(511, 168);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(681, 207);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "  Input  ";
@@ -65,9 +67,10 @@
             // button1
             // 
             this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(443, 135);
+            this.button1.Location = new System.Drawing.Point(591, 166);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(62, 23);
+            this.button1.Size = new System.Drawing.Size(83, 28);
             this.button1.TabIndex = 4;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
@@ -77,19 +80,21 @@
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(59, 136);
+            this.textBox2.Location = new System.Drawing.Point(79, 167);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(377, 20);
+            this.textBox2.Size = new System.Drawing.Size(501, 22);
             this.textBox2.TabIndex = 3;
             this.textBox2.TabStop = false;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(7, 136);
+            this.radioButton2.Location = new System.Drawing.Point(9, 167);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(41, 17);
+            this.radioButton2.Size = new System.Drawing.Size(51, 21);
             this.radioButton2.TabIndex = 2;
             this.radioButton2.Text = "File";
             this.radioButton2.UseVisualStyleBackColor = true;
@@ -97,20 +102,22 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(59, 28);
+            this.textBox1.Location = new System.Drawing.Point(79, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox1.Size = new System.Drawing.Size(446, 94);
+            this.textBox1.Size = new System.Drawing.Size(593, 115);
             this.textBox1.TabIndex = 1;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(7, 29);
+            this.radioButton1.Location = new System.Drawing.Point(9, 36);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.Size = new System.Drawing.Size(56, 21);
             this.radioButton1.TabIndex = 0;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Text";
@@ -122,18 +129,21 @@
             this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.dataGridView1);
-            this.groupBox2.Location = new System.Drawing.Point(13, 188);
+            this.groupBox2.Location = new System.Drawing.Point(17, 231);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(511, 180);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox2.Size = new System.Drawing.Size(681, 222);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "  Ciphers  ";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(442, 49);
+            this.button3.Location = new System.Drawing.Point(589, 60);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(62, 23);
+            this.button3.Size = new System.Drawing.Size(83, 28);
             this.button3.TabIndex = 6;
             this.button3.Text = "Remove";
             this.button3.UseVisualStyleBackColor = true;
@@ -141,9 +151,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(443, 20);
+            this.button2.Location = new System.Drawing.Point(591, 25);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(62, 23);
+            this.button2.Size = new System.Drawing.Size(83, 28);
             this.button2.TabIndex = 5;
             this.button2.Text = "Add...";
             this.button2.UseVisualStyleBackColor = true;
@@ -154,20 +165,22 @@
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.ColumnHeadersVisible = false;
-            this.dataGridView1.Location = new System.Drawing.Point(7, 20);
+            this.dataGridView1.Location = new System.Drawing.Point(9, 25);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(429, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(572, 185);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.TabStop = false;
             // 
             // button4
             // 
             this.button4.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button4.Location = new System.Drawing.Point(448, 376);
+            this.button4.Location = new System.Drawing.Point(597, 463);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(100, 28);
             this.button4.TabIndex = 2;
             this.button4.Text = "Close";
             this.button4.UseVisualStyleBackColor = true;
@@ -175,9 +188,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(367, 376);
+            this.button5.Location = new System.Drawing.Point(489, 463);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.Size = new System.Drawing.Size(100, 28);
             this.button5.TabIndex = 3;
             this.button5.Text = "Apply...";
             this.button5.UseVisualStyleBackColor = true;
@@ -194,16 +208,17 @@
             // Form1
             // 
             this.AcceptButton = this.button5;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.button4;
-            this.ClientSize = new System.Drawing.Size(536, 411);
+            this.ClientSize = new System.Drawing.Size(715, 506);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = ".:: Dasher ::.";
